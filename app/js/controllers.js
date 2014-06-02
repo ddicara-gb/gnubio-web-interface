58580f0cec88f04161d242e652b86df7f607540a
ddicara-gb <ddicara@gnubio.com>
2014-06-02 12:48:11 -0400

    Finally obtaining response from rest call and displaying in web interface.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,24 +1,14 @@
 'use strict';
 
 angular.module('MeltingTempApp.controllers', [])
-	.controller('tempsController2', ['$scope', 'meltingTempAPIservice2', function($scope, meltingTempAPIservice2) {
-		// Instantiate an object to store your scope data in (Best Practices)
-		$scope.data = {};
-		$scope.name = "name"
-	
-		meltingTempAPIservice2.get(function(response) {
-			// Assign the response INSIDE the callback
-			$scope.data.temp = response;
-		});
-	}])
 	.controller('tempsController', function($scope, meltingTempAPIservice) {
-		$scope.name = null;
-		$scope.sequence = null;
 		$scope.response = null;
+		$scope.sequence = null;
+		$scope.tm = null;
 
 		meltingTempAPIservice.getTemp().success(function (response) {
-			$scope.name = response.name;
-			$scope.sequence = response.sequence
-			$scope.response = response
+			$scope.response = response;
+			$scope.sequence = response['IDT'][0]['sequence'];
+			$scope.tm = response['IDT'][0]['melting_temp']['tm'];
 		});
 	});
--- app/js/services.js
+++ app/js/services.js
@@ -1,19 +1,17 @@
 'use strict';
 
 angular.module('MeltingTempApp.services', ['ngResource'])
-	.factory('meltingTempAPIservice2', function($resource){
-		return $resource('http://localhost:8010/MeltingTemps/Rest?name=CLR_WT&sequence=GTGGATCAAAGCCACT', {})
-	})
 	.factory('meltingTempAPIservice', function($http) {
-		
+	
 		var meltingTempAPI = {};
-
+	
 		meltingTempAPI.getTemp = function() {
 			return $http({
-				method: 'JSONP',
-				url: "http://localhost:8010/MeltingTemps/Rest?name=CLR_WT&sequence=GTGGATCAAAGCCACT"
+				method: 'GET',
+				url: 'http://localhost:8010/api/v1/MeltingTemperatures/IDT?sequence=ACCCGGGTTT',
+				mozSystem: true
 			});
 		}
-	return meltingTempAPI;
+		return meltingTempAPI;
 	})
 	.value('version', '0.1');
